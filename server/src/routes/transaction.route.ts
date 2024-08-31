f866b93e53d357000985a21372bd5941ddd66515
Ennygenius <inuenike@gmail.com>
2024-08-31 01:23:12 -0700

    testing get users recent transactions

--- server/src/routes/transaction.route.ts
+++ server/src/routes/transaction.route.ts
@@ -13,6 +13,6 @@ route
   .post("/send-coin", verifyToken, send_coin)
   .get("/transactions", getAllTransactions)
   .get("/transaction", verifyToken, getUserTransactions)
-  .get("/recent-transactions", getRecentUserTransactions);
+  .get("/recent-transactions", verifyToken, getRecentUserTransactions);
 
 export default route;
